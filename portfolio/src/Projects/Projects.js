0a25c0bb02e092f6a2974014f9e8b1e242442e54
RuaaAlsugheir <alsugheirruaa@gmail.com>
2021-08-24 15:11:28 +0300

    Initial setup for Projects component

---
+++ portfolio/src/Projects/Projects.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Projects() {
+  return (
+    <div className="projects">
+      <section className="project" id="Life_Stream"> // Project description
+        <button className="details">Details</button>
+      </section>
+      <section className="project" id="BooksWorms"> // Project description
+        <button className="details">Details</button>
+      </section>
+    </div>
+  );
+}
