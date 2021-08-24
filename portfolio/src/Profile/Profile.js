8d966296405fbbb057e305443fac5302aa5267d6
RuaaAlsugheir <alsugheirruaa@gmail.com>
2021-08-24 14:41:28 +0300

    Initial setup for Profile component

---
+++ portfolio/src/Profile/Profile.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Profile() {
+    return (
+        <div className="about_me">
+            <section className="skills"></section>
+            <section className="education_and_experience"></section>
+            <a>Download my CV</a>
+        </div>
+    )
+}
