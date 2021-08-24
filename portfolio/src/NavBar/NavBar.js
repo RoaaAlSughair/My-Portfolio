87539dd29eae6eb2245d8a8a19d3f099b302e446
RuaaAlsugheir <alsugheirruaa@gmail.com>
2021-08-24 14:51:41 +0300

    Initial setup for NavBar component

---
+++ portfolio/src/NavBar/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function NavBar() {
+    return (
+        <nav className="navbar">
+            <img src="" alt="Roaa AlSughair" className="logo"/> // Should have a link to home page
+            <a href="">Home</a>
+            <a href="">Profile</a>
+            <a href="">Projects</a>
+            <a href="">Contact</a>
+        </nav>
+    )
+}
